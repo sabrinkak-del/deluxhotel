0081da94d116e5af7cf8b5a716695065b8470ade
sabrinkak-del <sabrinka.k@gmail.com>
2026-02-04 20:11:47 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
+  root: __dirname,
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         booking: resolve(__dirname, 'booking.html'),
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './'),
     },
   },
 });
